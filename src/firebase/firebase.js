7159a8758f87b3eb9e5bc898983dfbf204bb8d8a
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2022-05-20 09:58:55 +0700

    NEW: congigure firebase auth

---
+++ src/firebase/firebase.js
@@ -0,0 +1,104 @@
+import { initializeApp } from "firebase/app";
+​​import {
+​​  GoogleAuthProvider,
+​​  getAuth,
+​​  signInWithPopup,
+​​  signInWithEmailAndPassword,
+​​  createUserWithEmailAndPassword,
+​​  sendPasswordResetEmail,
+​​  signOut,
+​​} from "firebase/auth";
+​​import {
+​​  getFirestore,
+​​  query,
+​​  getDocs,
+​​  collection,
+​​  where,
+​​  addDoc,
+​​} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRuS_fb_VutU47_Qy9xCeVxXNj2haaafU",
+    authDomain: "fir-auth-blog-1907a.firebaseapp.com",
+    projectId: "fir-auth-blog-1907a",
+    storageBucket: "fir-auth-blog-1907a.appspot.com",
+    messagingSenderId: "430785766956",
+    appId: "1:430785766956:web:1efe69b19652c701551389",
+    measurementId: "G-1P02TTW1EV"
+  };
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+};
+
+  const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const user = res.user;
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name,
+        authProvider: "local",
+        email,
+      });
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  const sendPasswordReset = async (email) => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert("Password reset link sent!");
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  const logout = () =>{
+      signOut(auth)
+  }
+
+  export {
+    auth,
+    db,
+    signInWithGoogle,
+    logInWithEmailAndPassword,
+    registerWithEmailAndPassword,
+    sendPasswordReset,
+    logout,
+  };
+
+  const a;
